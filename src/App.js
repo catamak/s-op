27a953609326c98fa091ae9fca9e960bbb6d45fc
catamak <kadircatamak@gmail.com>
2024-07-18 15:00:39 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
   };
 
   const generateCalendarData = (month, year) => {
-    
     const daysInMonth = new Date(year, month, 0).getDate();
     const calendarData = [
       ['Takvim', '', 'PVC Fabrikası', '', '', '', 'AYPE Fabrikası', '', '', '', '', '', '', ''],
@@ -46,39 +45,41 @@ function App() {
   };
 
   const customRenderer = (instance, td, row, col, prop, value, cellProperties) => {
-  const productionOptions = ['10', '20', '30', '40', '50'];
-  
-  const container = document.createElement('div');
-  container.style.display = 'flex';
-  container.style.flexDirection = 'column';
-  
-  const select = document.createElement('select');
-  select.className = 'htDropdownButton'; // CSS sınıfını ekleyin
-  productionOptions.forEach(optionValue => {
-    const option = document.createElement('option');
-    option.value = optionValue;
-    option.innerHTML = optionValue;
-    if (value === optionValue) {
-      option.selected = true;
-    }
-    select.appendChild(option);
-  });
-  select.addEventListener('change', () => {
-    instance.setDataAtCell(row, col, select.value);
-  });
-  
-  const input = document.createElement('input');
-  input.type = 'text';
-  input.value = ''; // Boş bırakıldı
-  
-  container.appendChild(select);
-  container.appendChild(input);
-  
-  td.innerHTML = '';
-  td.appendChild(container);
-  return td;
-};
+    const productionOptions = ['10', '20', '30', '40', '50'];
+    
+    const container = document.createElement('div');
+    container.style.display = 'flex';
+    container.style.flexDirection = 'column';
+    
+    const select = document.createElement('select');
+    select.className = 'htDropdownButton'; // CSS sınıfını ekleyin
+    productionOptions.forEach(optionValue => {
+      const option = document.createElement('option');
+      option.value = optionValue;
+      option.innerHTML = optionValue;
+      if (value === optionValue) {
+        option.selected = true;
+      }
+      select.appendChild(option);
+    });
+    select.addEventListener('change', () => {
+      instance.setDataAtCell(row, col, select.value);
+    });
+    
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = value;
+    input.addEventListener('change', () => {
+      instance.setDataAtCell(row, col, input.value);
+    });
 
+    container.appendChild(select);
+    container.appendChild(input);
+    
+    td.innerHTML = '';
+    td.appendChild(container);
+    return td;
+  };
 
   const colorRenderer = (instance, td, row, col, prop, value, cellProperties) => {
     const cellContent = document.createElement('div');
@@ -169,8 +170,8 @@ function App() {
           colHeaders={true}
           rowHeaders={true}
           mergeCells={[
-            { row: 0, col: 2, rowspan: 1, colspan: 4 }, // PVC Fabrikası
-            { row: 0, col: 6, rowspan: 1, colspan: 2 }  // AYPE Fabrikası
+            { row: 0, col: 2, rowspan: 1, colspan: 4 },
+            { row: 0, col: 6, rowspan: 1, colspan: 2 }
           ]}
           afterChange={afterChange}
           cells={(row, col) => {
